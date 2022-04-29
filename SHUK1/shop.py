d5318829d44f09669f8514eeb2a6c41959f9fb8d
theZohanDvir <dror.tomer.il@gmail.com>
2022-04-29 17:32:08 +0300

    first commit

--- SHUK1/shop.py
+++ SHUK1/shop.py
@@ -1,3 +1,8 @@
+from ast import For
+from SHUK1.stock import stock
+from SHUK1.stockItem import stockItem
+
+
 class shop:
     def __init__(self,market,founder):
         self.market = market
@@ -7,4 +12,13 @@ class shop:
         self.purchaseHistory = purchaseHistory(self) #load
         self.discountPolicy = discountPolicy(self)#load
         self.purchasePolicy = purchasePolicy(self)#load
-        self.stock = [] #load
+        self.stock = stock(self)
+
+    def addItemToStock(self,item : stockItem, stockNumber):
+        self.stock.addItemToStock(item,stockNumber)
+
+    def removeItemFromStock(self,item : stockItem):
+        self.stock.removeItemFromStock(item)
+        
+    def removeItemFromStock(self,itemID):
+        self.stock.removeItemFromStock(itemID)
--- SHUK1/stock.py
+++ SHUK1/stock.py
@@ -1,4 +1,34 @@
+from asyncio.windows_events import NULL
+from re import I
+from SHUK1.stockItem import stockItem
+
+
 class stock:
     def __init__(self,shop):
         self.shop = shop
         self.stockItems = []#load
+
+    def addItemToStock(self,item : stockItem, stockNumber):
+        flag = False
+        for i in self.stockItems:
+            if i[0].id == item.id:
+                flag = True
+                break
+        if flag == False:
+            self.stockItems.append((item,stockNumber))
+        else:
+            print("The item is already on the store stock," + item.printItem())
+
+    def removeItemFromStock(self,item : stockItem):
+        self.removeItemFromStock(itemID= item.id)
+
+    def removeItemFromStock(self,itemID):
+        tmp = NULL
+        for i in self.stockItems:
+            if i[0].id == itemID:
+                tmp = i
+                break
+        if tmp != NULL:
+            self.stockItems.remove(tmp)
+        else:
+            print("The item is'nt on the store stock, can't remove it!")
