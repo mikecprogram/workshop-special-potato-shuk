529507c7ba8c44a7d8fa626617dcdb63524cac45
Luee Akasha <luee@post.bgu.ac.il>
2022-05-04 03:23:23 +0300

    some attributes and methods have been added to DiscountPolicy.py

--- Dev/DomainLayer/Objects/DiscountPolicy.py
+++ Dev/DomainLayer/Objects/DiscountPolicy.py
@@ -4,4 +4,26 @@ from .Logger import Logger
 class DiscountPolicy:
 
     def ___init___(self):
-        pass
+        self._membersItemVsDiscount = None
+        self._guestsItemVsDiscount = None
+        raise NotImplementedError
+
+    def add_guest_discount(self, itemId, newDiscount):
+        raise NotImplementedError
+
+    def update_guest_discount(self, itemId, new_Discount):
+        raise NotImplementedError
+
+    def cancel_guest_discount(self,itemId):
+        raise NotImplementedError
+
+    def add_member_discount(self, itemId, newDiscount):
+        raise NotImplementedError
+
+    def update_member_discount(self, itemId, new_Discount):
+        raise NotImplementedError
+
+    def cancel_member_discount(self,itemId):
+        raise NotImplementedError
+
+
