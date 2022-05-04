98f213830c15e8ad408dd9aebeb654fca21ac042
Luee Akasha <luee@post.bgu.ac.il>
2022-05-04 03:24:27 +0300

    some attributes have been added to StockItem.py

--- Dev/DomainLayer/Objects/StockItem.py
+++ Dev/DomainLayer/Objects/StockItem.py
@@ -3,5 +3,11 @@ from .Logger import Logger
 
 class StockItem:
 
-    def ___init___(self):
+    def ___init___(self, itemId, itemName: str):
+        self._name = None
+        self._itemId = None
         pass
+
+
+
+
