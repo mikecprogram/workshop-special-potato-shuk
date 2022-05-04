276c7c3cbdb0b742fed0b59b124eeec1f9a661a1
Luee Akasha <luee@post.bgu.ac.il>
2022-05-04 03:23:59 +0300

    some attributes and methods have been added to Shop.py

--- Dev/DomainLayer/Objects/Shop.py
+++ Dev/DomainLayer/Objects/Shop.py
@@ -1,8 +1,49 @@
 from .Logger import Logger
-
-
+import Stock
+import StockItem
+import Member
+import PurchasePolicy
+import DiscountPolicy
+import PurchaseHistory
+import Assignment
 
 class Shop:
 
-    def ___init___(self):
+    def ___init___(self,shopName : str,  founder : Member):
+        self._name = shopName
+        self._stock = Stock()
+        self._status = None   # need to confirm if we need shop's status such as closed/open.
+        self._founder = founder
+        self._owners = None
+        self._managers  = None
+        self._purchasePolicy = None
+        self._discountPolicy = DiscountPolicy()
+        self._purchaseHistory = None
+        self._assignments = None
+        pass # we may need more attributes
+
+    def add_item(self):
         pass
+
+    def remove_item(self):
+        pass
+
+    def update_purchase_policy(self):
+        pass
+
+    def update_discount_policy(self):
+        pass
+
+    def assign_owner(self):
+        pass
+
+    def assign_manager(self):
+        pass
+
+    def close_shop(self):
+        pass
+
+
+
+
+
