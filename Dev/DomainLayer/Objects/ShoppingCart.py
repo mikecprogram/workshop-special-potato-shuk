4c3a1558ac2ad81ee7a819dad9ea4854ffc38fdb
Luee Akasha <luee@post.bgu.ac.il>
2022-05-04 03:24:37 +0300

    some attributes and methods have been added to ShoppingCart.py

--- Dev/DomainLayer/Objects/ShoppingCart.py
+++ Dev/DomainLayer/Objects/ShoppingCart.py
@@ -4,4 +4,13 @@ from .Logger import Logger
 class ShoppingCart:
 
     def ___init___(self):
+        self._cartOwner = None
+        self._shoppingBaskets = None
+        self._cartPrice = None
         pass
+
+    def purchase_cart(self):
+        pass
+
+
+
