72a4a477f659eacc11a69516efa770ef9268a1a3
Luee Akasha <luee@post.bgu.ac.il>
2022-05-04 03:23:47 +0300

    some attributes and methods have been added to Market.py

--- Dev/DomainLayer/Objects/Market.py
+++ Dev/DomainLayer/Objects/Market.py
@@ -4,4 +4,19 @@ from .Logger import Logger
 class Market:
 
     def ___init___(self):
+        self._shops = None
+        self._members = None
+        self._onlineVisitors = None
         pass
+
+
+    def register_member(self):
+        pass
+
+    def open_shop(self):
+        pass
+
+    def close_shop(self):
+        pass
+
+
