a3fab618c21c507fe49cfdc3dab7e3379fac2fc5
Luee Akasha <luee@post.bgu.ac.il>
2022-05-04 03:22:49 +0300

    some attributes have been added to Assignment.py

--- Dev/DomainLayer/Objects/Assignment.py
+++ Dev/DomainLayer/Objects/Assignment.py
@@ -3,4 +3,8 @@ from .Logger import Logger
 class Assignment:
 
     def ___init___(self):
+        self._assigner = None
+        self._assignee = None
+        self._assignmentType = None
         pass
+
