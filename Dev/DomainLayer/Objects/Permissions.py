967821b1f5e047bac6a890d5daada9d5be55d037
Luee Akasha <luee@post.bgu.ac.il>
2022-05-24 15:19:34 +0300

    usecase completed!

--- Dev/DomainLayer/Objects/Permissions.py
+++ Dev/DomainLayer/Objects/Permissions.py
@@ -4,4 +4,7 @@ from .Logger import Logger
 class Permissions:
 
     def __init__(self):
-        pass
+        self._managerAssignment = None # boolean
+
+    def can_assign_manager(self):
+        return self._managerAssignment
--- Dev/DomainLayer/Objects/Shop.py
+++ Dev/DomainLayer/Objects/Shop.py
@@ -15,13 +15,13 @@ class Shop():
         self._stock = Stock
         self._status = None  # need to confirm if we need shop's status such as closed/open. TODO
         self._founder = founder
-        self._owners = []  # [username, ..]
-        self._managers = []
+        self._owners = {}  # {ownerUsername, Member}
+        self._managers = {}  # {managerUsername, Member}
         self._purchasePolicy = []
         self._discountPolicy = DiscountPolicy()
         self._purchaseHistory = PurchaseHistory()
-        self._assignments = {}  # hashmap {assigner, [assignees]}
-
+        self._owners_assignments = {}
+        self._managers_assignments = {}
         pass
 
     def add_item(self, item: StockItem):
@@ -41,14 +41,34 @@ class Shop():
             self._owners.append(assignee)
             self._assignments[assigner].append(Assignment(assigner, assignee))
 
-    def assign_manager(self, assigner, assignee):
-        if assignee not in self._managers:
-            self._managers.append(assignee)
-            self._assignments[assigner].append(Assignment(assigner, assignee))
+    def assign_manager(self, assignerUsername, assignee):
+        if self.is_owner(assignerUsername):
+            self._managers[assignerUsername] = assignee
+            self.add_assignment(assignerUsername, assignee.get_username(), self._managers_assignments)
+        elif self.is_manager(assignerUsername):
+            if self._managers[assignerUsername].can_assign_manager():
+                self._managers[assignee.get_username()] = assignee
+                self.add_assignment(assignerUsername, assignee.get_username(), self._managers_assignments)
+            else:
+                raise Exception("Assigner manager does not have the permission to assign managers!")
+        else:
+            raise Exception("Manager assignment failed!")
+        return True
 
+    def add_assignment(self, assignerUsername, assigneeUsername, assignment):
+        if assignment[assignerUsername] is not None:
+            assignment[assignerUsername].append(Assignment(assignerUsername, assigneeUsername))
+        else:
+            assignment[assignerUsername] = [Assignment(assignerUsername, assigneeUsername)]
 
     def close_shop(self):
         pass  # just change state of shop to closed TODO
+
+    def is_manager(self, managerUsername):
+        return self._managers[managerUsername] is not None
+
+    def is_owner(self, ownerUsername):
+        return self._owners[ownerUsername] is not None or self._founder.get_username() == ownerUsername
 
     def getTotalDiscount(self, user):
         totaldiscount = 1
