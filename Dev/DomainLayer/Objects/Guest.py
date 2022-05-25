67b3942ea0afe0402f0f7b4642715e4cd2d2fa7e
Luee Akasha <luee@post.bgu.ac.il>
2022-05-26 00:07:22 +0300

    fixed issue!

--- Dev/DomainLayer/Objects/Guest.py
+++ Dev/DomainLayer/Objects/Guest.py
@@ -1,19 +1,13 @@
-from .Logger import Logger
-from .Member import Member
-from User import User
+##from .Logger import Logger
 
 
 class Guest:
 
-    def __init__(self, user: User):
+    def __init__(self, user):
         self._user = user
 
-    def logout(self):
-        # should do nothing on logout by definition
-        pass
+    def exit(self): # does not do anything
+        return True
 
-    def login(self, marketid, username, password):
-        pass
-
-    def get_username(self):
-        return self._username
+    def assign_owner(self, shopName, memberToAssign):
+        raise Exception("Guest could not assign shop owners")
