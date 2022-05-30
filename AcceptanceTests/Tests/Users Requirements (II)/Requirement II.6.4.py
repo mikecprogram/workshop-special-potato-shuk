3709913a0aaba0ea4bf4a974ca6ade110270afd2
Luee Akasha <luee@post.bgu.ac.il>
2022-05-30 16:49:02 +0300

    some errors were resolved!

--- AcceptanceTests/Tests/Users Requirements (II)/Requirement II.6.4.py
+++ AcceptanceTests/Tests/Users Requirements (II)/Requirement II.6.4.py
@@ -1,9 +1,11 @@
 import unittest
 import sys
 #this is how you import from different folder in python:
+from Dev.ServiceLayer.SystemService import SystemService
+
 sys.path.insert(0, r'C:\Users\user\Desktop\workshop-special-potato-shuk\dev\ServiceLayer')
 
-from SystemService import *
+from Dev.ServiceLayer.SystemService import *
 
 
 class MyTestCase(unittest.TestCase):
